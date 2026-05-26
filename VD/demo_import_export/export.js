32f867d5b3ef960b704cc8445cda006c12aeaa03
vuductien08042006-rgb <vuductien08042006@gmail.com>
2026-05-26 12:10:21 +0700

    remove unwanted folders

--- VD/demo_import_export/export.js
+++
@@ -1,5 +0,0 @@
-const PI=3.14;
-funcion hinhtron(radius){
-    return PI *radius*radius;
-}
-export{PI,hinhtron}
--- VD/demo_import_export/import.js
+++
@@ -1,3 +0,0 @@
-import{PI,hinhtron} form './export.js';
-console.log(PI);
-console.log(hinhtron(5));
